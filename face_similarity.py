b4cd110e7ea0107336c50de09b2d8bcf9e408aa4
Sheldon <sheldon0789@gmail.com>
2019-04-09 15:53:05 -0400

    Finished getting similarity

--- face_similarity.py
+++ face_similarity.py
@@ -1,9 +1,15 @@
 import face_recognition
 from glob import glob
+import csv
+
+csv_file = open('similarity.csv', mode='w')
+csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+csv_writer.writerow(['image','similarity'])
 
 folder_path = 'big_images/'
 
 similarity_list = []
+count = 0
 for image_name in sorted(glob(folder_path + "*.jpg")):
     image = face_recognition.load_image_file(image_name)
     face_encodings = face_recognition.face_encodings(image)
@@ -12,7 +18,18 @@ for image_name in sorted(glob(folder_path + "*.jpg")):
         sim_score = face_recognition.face_distance([face_encodings[0]], face_encodings[1])[0]
         similarity_list.append({ "name" : image_name, "similarity" : sim_score })
 
+    count += 1
+    if count % 20 == 0:
+        print ("Processed ", count, " images")
 sorted_similarity = sorted(similarity_list, key = lambda img : img['similarity'])
 
-for imgdata in sorted_similarity[:20]:
-    print (imgdata['name'], sim_score)
+print ("===Most Similar===")
+for imgdata in sorted_similarity[:10]:
+    print (imgdata['name'], imgdata['similarity'])
+
+print ("\n\n===Least Similar===")
+for imgdata in sorted_similarity[-10:]:
+    print (imgdata['name'], imgdata['similarity'])
+
+for imgdata in sorted_similarity:
+    csv_writer.writerow([imgdata['name'], imgdata['similarity']])
